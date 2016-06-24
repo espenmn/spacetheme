355af3c813a058e4aabe53abf8a37cea9e7d5d6a
Espen Moe-Nilssen <espenmoe-nilssen@Espen.lan>
2016-06-24 12:32:38 +0200

    detab interfaces.py

--- medialog.spacetectheme/src/medialog/spacetectheme/interfaces.py
+++ medialog.spacetectheme/src/medialog/spacetectheme/interfaces.py
@@ -3,6 +3,12 @@
 
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 from plone.app.portlets.interfaces import IColumn
+    
+from z3c.form import interfaces
+from zope import schema
+from zope.interface import alsoProvides
+from plone.directives import form
+from medialog.controlpanel.interfaces import IMedialogControlpanelSettingsProvider
 
 
 class IMedialogSpacetecthemeLayer(IDefaultBrowserLayer):
@@ -12,27 +18,20 @@ class IBelow(IColumn):
     """here we put the below content portlets
     """    
 
-from plone.theme.interfaces import IDefaultPloneLayer
-    
-from z3c.form import interfaces
-from zope import schema
-from zope.interface import alsoProvides
-from plone.directives import form
-from medialog.controlpanel.interfaces import IMedialogControlpanelSettingsProvider
 
 
 class ISpacetecSettings(form.Schema):
-	"""Adds settings to medialog.controlpanel
-	"""
+    """Adds settings to medialog.controlpanel
+    """
 
-	form.fieldset(
-		'frontslider',
-		label=_(u'frontslder'),
-		fields=[
-			'transaction',
-			'speed',
-		],
-	)
+    form.fieldset(
+        'frontslider',
+        label=_(u'frontslder'),
+        fields=[
+            'transaction',
+            'speed',
+        ],
+    )
 
     speed = schema.Int(
             title=_(u"Speed"),
