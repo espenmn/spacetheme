cbd9e86ebcca719b41d528257d621e79bc73d70c
Espen Moe-Nilssen <espenmoe-nilssen@Espen.lan>
2016-06-28 18:30:48 +0200

    another try

--- medialog.spacetectheme/src/medialog/spacetectheme/theme/javascript/slides.js
+++ medialog.spacetectheme/src/medialog/spacetectheme/theme/javascript/slides.js
@@ -1,22 +1,18 @@
 $( document ).ready(function() {
     $("#slider").responsiveSlides({
     maxwidth: 1400,
-    maxheight: 450,
     nav: false,
-    speed:   $('#main-slider').attr("data-speed"),
-    timeout: $('#main-slider').attr("data-timeout"),
-    pager: false,
-    prevText: "<",  
-    nextText: ">",    
+    speed:   $(this).attr("data-speed"),
+    timeout: $(this).attr("data-timeout"),
+    pager: false, 
     });
-	$(".sliderportlet").responsiveSlides({
+    $(".sliderportlet").responsiveSlides({
     maxwidth: 768,
     nav: true,
-    speed:   $('.sliderportlet').attr("data-speed"),
-    timeout: $('.sliderportlet').attr("data-timeout"),
+    speed:   $(this).attr("data-speed"),
+    timeout: $(this).attr("data-timeout"),
     pager: false,
     prevText: "<",  
     nextText: ">",    
-    namespace: "srslides",  
     });
 });
